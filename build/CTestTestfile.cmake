14e409b73c193b4173fa9b81bf3ac4b4e906c986
Julian Franko <jufr1176@colorado.edu>
2024-01-25 05:17:35 +0000

    adding all files to github

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jovyan/aoc
+# Build directory: /home/jovyan/aoc/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
