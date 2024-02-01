bd569d6608a18c3a63e10651d8ac5feab1e4bbc0
Julian Franko <jufr1176@colorado.edu>
2024-02-01 04:26:41 +0000

    cmake configuration

---
+++ build/CMakeFiles/aoc.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jovyan/aoc/src/character.cpp" "CMakeFiles/aoc.dir/src/character.o" "gcc" "CMakeFiles/aoc.dir/src/character.o.d"
+  "/home/jovyan/aoc/src/game.cpp" "CMakeFiles/aoc.dir/src/game.o" "gcc" "CMakeFiles/aoc.dir/src/game.o.d"
+  "/home/jovyan/aoc/src/main.cpp" "CMakeFiles/aoc.dir/src/main.o" "gcc" "CMakeFiles/aoc.dir/src/main.o.d"
+  "/home/jovyan/aoc/src/player.cpp" "CMakeFiles/aoc.dir/src/player.o" "gcc" "CMakeFiles/aoc.dir/src/player.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/aoc.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aoc.dir/src/character.o"
+  "CMakeFiles/aoc.dir/src/character.o.d"
+  "CMakeFiles/aoc.dir/src/game.o"
+  "CMakeFiles/aoc.dir/src/game.o.d"
+  "CMakeFiles/aoc.dir/src/main.o"
+  "CMakeFiles/aoc.dir/src/main.o.d"
+  "CMakeFiles/aoc.dir/src/player.o"
+  "CMakeFiles/aoc.dir/src/player.o.d"
+  "aoc"
+  "aoc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
